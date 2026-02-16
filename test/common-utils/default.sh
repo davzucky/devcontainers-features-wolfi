8472769cedfd0fa2b1e560dbd00867e9b2c8de21
David Zucker <david@programmableproduction.com>
2026-02-16 14:39:35 +0900

    Set zsh as default shell by default in common-utils (#71)

--- test/common-utils/default.sh
+++ test/common-utils/default.sh
@@ -10,6 +10,7 @@ check "iconv installed" command -v iconv
 check "bat installed" command -v bat
 check "nvim installed" command -v nvim
 check "zsh installed" command -v zsh
+check "zsh default shell for vscode" bash -c "awk -F: '\$1==\"vscode\"{print \$7}' /etc/passwd | grep -qE '/zsh$'"
 check "default user exists" id -u vscode
 check "default user in sudo group" bash -lc "id -nG vscode | tr ' ' '\\n' | grep -qx sudo"
 check "oh-my-zsh installed for vscode" test -d /home/vscode/.oh-my-zsh
