8f982afac599303301cfabaa46650361882f1bf6
David Zucker <david@programmableproduction.com>
2026-04-10 12:19:22 +0900

    Fix docker-in-docker DNS and /tmp mount conflicts (#90) (#91)
    * Fix docker-in-docker DNS and /tmp mount conflicts (#90)
    
    * Fix docker-in-docker Azure DNS test path (#90)

--- test/docker-in-docker/default.sh
+++ test/docker-in-docker/default.sh
@@ -5,7 +5,7 @@ set -e
 source dev-container-features-test-lib
 
 check "docker version" docker --version
-check "docker init script" ls -l /usr/local/share/docker-in-docker-init.sh
+check "docker init script" ls -l /usr/local/share/docker-init.sh
 check "docker ps" docker ps
 check "docker build" docker build ./
 check "docker buildx" docker buildx version
