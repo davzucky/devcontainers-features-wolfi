148e8666af3d5752ce33d8e36871002d1d8fc562
David Zucker <david@programmableproduction.com>
2026-04-10 12:14:48 +0900

    Fix docker-in-docker Azure DNS test path (#90)

--- test/docker-in-docker/azure_dns_autodetection_false.sh
+++ test/docker-in-docker/azure_dns_autodetection_false.sh
@@ -4,6 +4,6 @@ set -e
 
 source dev-container-features-test-lib
 
-check "azure dns disabled in init" bash -c "grep -F \"AZURE_DNS_AUTO_DETECTION='false'\" /usr/local/share/docker-in-docker-init.sh"
+check "azure dns disabled in init" bash -c "grep -F \"AZURE_DNS_AUTO_DETECTION='false'\" /usr/local/share/docker-init.sh"
 
 reportResults
